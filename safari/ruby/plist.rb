ae374fce1056e97a06c3c58f0dc771594c4994b8
Tom Ashworth <tom@phuu.net>
2012-12-12 12:55:01 +0000

    Getting data back from the inspector with Ruby!

---
+++ safari/ruby/plist.rb
@@ -0,0 +1,40 @@
+require 'socket'
+require 'CFPropertyList'
+require 'uuid'
+
+uuid = UUID.new
+
+s = Socket.new Socket::PF_INET6, Socket::SOCK_STREAM
+s.connect Socket.pack_sockaddr_in(27753, '::1')
+
+def send(s, data)
+  plist = CFPropertyList::List.new
+  plist.value = CFPropertyList.guess(data)
+  s.write [plist.to_str.length].pack('L>')
+  s.write plist.to_str
+end
+
+id = uuid.generate
+
+puts 'Connected'
+
+send(s, {
+  '__argument' => {
+    'WIRConnectionIdentifierKey' => id
+  },
+  '__selector' => '_rpc_reportIdentifier:'
+})
+
+# send(s, {
+#   '__argument' => {
+#     'WIRConnectionIdentifierKey' => id,
+#     'WIRConnectionIdentifierKey' => 'com.apple.mobilesafari'
+#   },
+#   '__selector' => '_rpc_forwardGetListing:'
+# })
+
+while line = s.gets # Read lines from socket
+  puts line         # and print them
+end
+
+s.close             # close socket when done
